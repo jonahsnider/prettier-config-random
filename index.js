00d433a6d32f32546f8f8d79c38b7402fcb9adf9
Jonah Snider <jonah@jonahsnider.com>
2021-05-03 20:33:47 -0700

    feat: change values each time they are read

--- index.js
+++ index.js
@@ -6,20 +6,52 @@ const boolean = [false, true];
 
 /** @type {import('prettier').Options} */
 module.exports = {
-	printWidth: sample([80, 100, 120, 140, 160]),
-	tabWidth: sample([2, 4, 6, 8]),
-	useTabs: sample(boolean),
-	semi: sample(boolean),
-	singleQuote: sample(boolean),
-	quoteProps: sample(["as-needed", "consistent", "preserve"]),
-	jsxSingleQuote: sample(boolean),
-	trailingComma: sample(["es5", "none", "all"]),
-	bracketSpacing: sample(boolean),
-	jsxBracketSameLine: sample(boolean),
-	arrowParens: sample(["avoid", "always"]),
-	insertPragma: sample(boolean),
-	proseWrap: sample(["always", "never", "preserve"]),
-	htmlWhitespaceSensitivity: sample(["css", "strict", "ignore"]),
-	vueIndentScriptAndStyle: sample(boolean),
-	endOfLine: sample(["lf", "crlf", "cr"]),
+	get printWidth() {
+		return sample([80, 100, 120, 140, 160]);
+	},
+	get tabWidth() {
+		return sample([2, 4, 6, 8]);
+	},
+	get useTabs() {
+		return sample(boolean);
+	},
+	get semi() {
+		return sample(boolean);
+	},
+	get singleQuote() {
+		return sample(boolean);
+	},
+	get quoteProps() {
+		return sample(["as-needed", "consistent", "preserve"]);
+	},
+	get jsxSingleQuote() {
+		return sample(boolean);
+	},
+	get trailingComma() {
+		return sample(["es5", "none", "all"]);
+	},
+	get bracketSpacing() {
+		return sample(boolean);
+	},
+	get jsxBracketSameLine() {
+		return sample(boolean);
+	},
+	get arrowParens() {
+		return sample(["avoid", "always"]);
+	},
+	get insertPragma() {
+		return sample(boolean);
+	},
+	get proseWrap() {
+		return sample(["always", "never", "preserve"]);
+	},
+	get htmlWhitespaceSensitivity() {
+		return sample(["css", "strict", "ignore"]);
+	},
+	get vueIndentScriptAndStyle() {
+		return sample(boolean);
+	},
+	get endOfLine() {
+		return sample(["lf", "crlf", "cr"]);
+	},
 };
